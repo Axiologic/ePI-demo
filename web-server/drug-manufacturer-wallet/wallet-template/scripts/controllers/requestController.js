176e2da72b96be267f745c9baa04d28935370213
skutner <sava.dumitru.daniel@gmail.com>
2020-06-24 19:55:06 +0300

    updated outbox page

---
+++ web-server/drug-manufacturer-wallet/wallet-template/scripts/controllers/requestController.js
@@ -0,0 +1,16 @@
+import ContainerController from "../../cardinal/controllers/base-controllers/ContainerController.js";
+
+export default class requestController extends ContainerController {
+    constructor(element, history) {
+        super(element);
+        window.addEventListener("send-leaflet", (event) => {
+            const newEvent = new Event("send-leaflet");
+            newEvent.data = event.data;
+            let profileSsapp = element.querySelector("#profileSsapp iframe");
+            profileSsapp.contentWindow.addEventListener("leaflet-sent", (event) => {
+                history.push("/messages/outbox")
+            });
+            profileSsapp.contentWindow.dispatchEvent(newEvent);
+        });
+    }
+}
